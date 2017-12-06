a0e5250944307a715816c64a371ba59cb7fb2289
boske <goran.bosic@openratio.com>
2017-12-06 15:41:27 +0100

    all

--- resources/views/front/club/profile.blade.php
+++ resources/views/front/club/profile.blade.php
@@ -76,19 +76,19 @@
 
     <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
     <!-- Theme style -->
-    <link href="{{ asset('assets/js/jquery-chat/templates/AdminLTE/dist/css/AdminLTE.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/js/jquery-chat-master/templates/AdminLTE/dist/css/AdminLTE.css')}}" rel="stylesheet" type="text/css" />
 
     <link id='theme' rel='stylesheet' />
-    <link rel='stylesheet' href={{ asset('assets/js/css/tipsy.css')}} />
-    <link rel='stylesheet' href={{ asset('assets/js/css/chat.css')}} />
+    <link rel='stylesheet' href={{ asset('assets/js/jquery-chat-master/css/tipsy.css')}} />
+    <link rel='stylesheet' href={{ asset('assets/js/jquery-chat-master/css/chat.css')}} />
 
     <script src='https://jquery-chat.herokuapp.com/socket.io/socket.io.js'></script>
-    <script src={{ asset('assets/js/jquery-chat/js/jquery-1.11.3.min.js')}}></script>
-    <script src={{ asset('assets/js/jquery-chat/js/jquery-ui-1.10.4.custom.min.js')}}></script>
-    <script src={{ asset('assets/js/jquery-chat/js/jquery.slimscroll.min.js')}}></script>
-    <script src={{ asset('assets/js/jquery-chat/js/jquery.tipsy.js')}}></script>
-    <script src={{ asset('assets/js/jquery-chat/js/jquery.main.js')}}></script>
-    <script src={{ asset('assets/js/jquery-chat/config.js')}}></script>
-    <script src={{ asset('assets/js/jquery-chati18n_en.js')}}></script>
+    <script src={{ asset('assets/js/jquery-chat-master/js/jquery-1.11.3.min.js')}}></script>
+    <script src={{ asset('assets/js/jquery-chat-master/js/jquery-ui-1.10.4.custom.min.js')}}></script>
+    <script src={{ asset('assets/js/jquery-chat-master/js/jquery.slimscroll.min.js')}}></script>
+    <script src={{ asset('assets/js/jquery-chat-master/js/jquery.tipsy.js')}}></script>
+    <script src={{ asset('assets/js/jquery-chat-master/js/jquery.main.js')}}></script>
+    <script src={{ asset('assets/js/jquery-chat-master/config.js')}}></script>
+    <script src={{ asset('assets/js/jquery-chat-master/i18n_en.js')}}></script>
 
 @stop
