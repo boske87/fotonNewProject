2eb9a5b8ac22a2f24c92491c9b15e52519b927c0
boske <goran.bosic@openratio.com>
2018-07-24 11:43:53 +0200

    new

---
+++ app/Http/Controllers/UserExtraController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class UserExtraController extends Controller
+{
+    public function index($id){
+        $user = User::find($id);
+        $myGal = $user->userGallery;
+        $callGal = $user->callGallery;
+
+        return view('front.club.otherView.profile', compact('callGal', 'myGal', 'user'));
+
+    }
+}
