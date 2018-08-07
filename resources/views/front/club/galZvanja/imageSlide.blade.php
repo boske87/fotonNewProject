e4fa481675089cd9b01d72f0eb4a43b6ffc4d927
boske <goran.bosic@openratio.com>
2018-08-07 09:59:37 +0200

    new

--- resources/views/front/club/galZvanja/imageSlide.blade.php
+++ resources/views/front/club/galZvanja/imageSlide.blade.php
@@ -55,8 +55,9 @@
 
                     <li class="color-circles">
                         <ul>
-                            <li style="background-color:{{$gallery->color}}"></li>
-                            <li class="txt"><span>{{$gallery->titula}}</span></li>
+                            <li style="background-color:{{$gallery->color}} ;width: 60px; height: 60px;line-height: 60px;
+                                    text-align: center;">{{Auth::user()->titula}}</li>
+                            <li class="txt"></li>
                         </ul>
                     </li>
                 </ul>
@@ -87,8 +88,9 @@
                                 </li>
                                 <li class="color-circles">
                                     <ul>
-                                        <li style="background-color:{{Auth::user()->color}}"></li>
-                                        <li class="txt" style="width: 50%; text-align: left"><span>{{Auth::user()->titula}}</span></li>
+                                        <li  style="background-color:{{Auth::user()->color}}; width: 40px; height: 40px;line-height: 40px;
+                                                text-align: center;">{{Auth::user()->titula}}</li>
+                                        <li class="txt" style="width: 50%; text-align: left"></li>
                                     </ul>
                                 </li>
                             </ul>
@@ -134,8 +136,8 @@
                                 '</li>\n' +
                                 '<li class="color-circles">\n' +
                                 '<ul>\n' +
-                                '<li style="background-color:'+data.result[x].color+';"></li>\n' +
-                                ' <li class="txt" style="width: 50%; text-align: left"><span>'+data.result[x].titula+'</span></li>\n' +
+                                '<li style="background-color:'+data.result[x].color+'; width: 40px; height: 40px;line-height: 40px;text-align: center;">'+data.result[x].titula+'</li>\n' +
+                                ' <li class="txt" style="width: 50%; text-align: left;"></li>\n' +
                                 '</ul>\n' +
                                 '</li>\n' +
                                 '</ul>\n' +
@@ -181,8 +183,8 @@
                         '</li>\n' +
                         '<li class="color-circles">\n' +
                         '<ul>\n' +
-                        '<li style="background-color:{{Auth::user()->color}};"></li>\n' +
-                        ' <li class="txt" style="width: 50%; text-align: left"><span>{{Auth::user()->titula}}</span></li>\n' +
+                        '<li style="background-color:{{Auth::user()->color}};width: 40px; height: 40px;line-height: 40px;text-align: center;">{{Auth::user()->titula}}</li>\n' +
+                        ' <li class="txt" style="width: 50%; text-align: left"></li>\n' +
                         '</ul>\n' +
                         '</li>\n' +
                         '</ul>\n' +
